d2e89981811423a79f1470da42de4300de805430
山崎孝洋 <tak_3_zaki3@yahoo.co.jp>
2017-02-06 18:30:14 +0900

    start study

---
+++ prog.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	width, height = 600, 320
+	cells         = 50
+	xyrange       = 30.0
+	xyscale       = width / 2 / xyrange
+	zscale        = height * 0.4
+	angle         = math.Pi / 6
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle)
+
+func main() {
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+j, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+j, j+1)
+			if isEnable(ax, ay, bx, by, cx, cy, dx, dy) {
+				fmt.Printf("<polygon points='%g,%g,%g,%g,%g,%g,%g,%g'/>\n",
+					ax, ay, bx, by, cx, cy, dx, dy)
+			}
+		}
+	}
+	fmt.Println("</svg>")
+}
+func isEnable(vals ...float64) bool {
+	for r := range vals {
+		if math.IsNaN(vals[r]) {
+			return false
+		}
+	}
+	return true
+}
+
+func corner(i, j int) (float64, float64) {
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+	z := f(x, y)
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy
+}
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y)
+	return math.Sin(r) / r
+}
